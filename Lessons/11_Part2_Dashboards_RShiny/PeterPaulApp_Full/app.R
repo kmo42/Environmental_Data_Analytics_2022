98598fce41fc1599d0e286aed5f0ea335fcac3de
kmo42 <kmo42@duke.edu>
2022-04-10 07:40:33 -0400

    End of A11 crafting reports, including rough knitting patch with John helping me knit A11; A11 RShiny; made mock up of project to reference TS work from class

--- Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/app.R
+++ Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/app.R
@@ -11,7 +11,7 @@ nutrient_data <- nutrient_data %>%
   select(lakename, sampledate:po4)
 
 #### Define UI ----
-ui <- fluidPage(theme = shinytheme("yeti"),
+ui <- fluidPage(theme = shinytheme("readable"),
   titlePanel("Nutrients in Peter Lake and Paul Lake"),
   sidebarLayout(
     sidebarPanel(
@@ -63,12 +63,12 @@ server <- function(input, output) {
         ggplot(filtered_nutrient_data(), 
                aes_string(x = "sampledate", y = input$y, 
                           fill = "depth_id", shape = "lakename")) +
-          geom_point(alpha = 0.8, size = 2) +
+          geom_point(alpha = 0.6, size = 4) +
           theme_classic(base_size = 14) +
           scale_shape_manual(values = c(21, 24)) +
           labs(x = "Date", y = expression(Concentration ~ (mu*g / L)), shape = "Lake", fill = "Depth ID") +
-          scale_fill_distiller(palette = "YlOrBr", guide = "colorbar", direction = 1)
-          #scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
+          #scale_fill_distiller(palette = "YlOrBr", guide = "colorbar", direction = 1)
+          scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
       })
        
     # Create a table that generates data for each point selected on the graph  
@@ -84,18 +84,64 @@ shinyApp(ui = ui, server = server)
 
 #### Questions for coding challenge ----
 #1. Play with changing the options on the sidebar. 
-    # Choose a shinytheme that you like. The default here is "yeti"
-    # How do you change the default settings? 
-    # How does each type of widget differ in its code and how it references the dataframe?
+
+# Choose a shinytheme that you like. The default here is "yeti"
+     ## Chose "readable".
+
+# How do you change the default settings? 
+     ## Google how to change default settings, and pick a shinyoptions(). To change 
+     ## the theme, first you must download shinythemes, then change the part in 
+     ## quotes on the fluidpage line to desired theme. 
+
+# How does each type of widget differ in its code and how it references the dataframe? 
+     ## One widget has a select dropdown menu, another has a select depth box to check, 
+     ## a third has a select box for which lake, and the fourth has a sliding date bar 
+     ## to choose a date range.
+
 #2. How is the mainPanel component of the UI structured? 
-    # How does the output appear based on this code?
+     ## It runs the data, and functions in the widgets and pops out a scatterplot 
+     ## with different colored points with color based on depth. Time is on the 
+     ## x-axis and concentrations is on the y axis. 
+
+# How does the output appear based on this code? 
+     ## The output is a total phosphorus scatterplot with dark red, orange, and 
+     ## white triangles and circles points for Peter and Paul Lake. 
+
 #3. Explore the reactive formatting within the server.
-    # Which variables need to have reactive formatting? 
-    # How does this relate to selecting rows vs. columns from the original data frame?
+
+# Which variables need to have reactive formatting? 
+     ## The nutrient variable, depth, lake, and date need to have reactive formatting, 
+     ## plus with fluidpage the outputs will form to whatever shape the output window 
+     ## is set to be. 
+
+# How does this relate to selecting rows vs. columns from the original data frame? 
+     ## Nutrient type will select a column, depth will pull rows from the depth 
+     ## column, lake will select a column, and data will select row range from 
+     ## sampledate column. 
+
 #4. Analyze the similarities and differences between ggplot code for a rendered vs. static plot.
-    # Why are the aesthetics for x, y, fill, and shape formatted the way they are?
-    # Note: the data frame has a "()" after it. This is necessary for reactive formatting.
-    # Adjust the aesthetics, playing with different shapes, colors, fills, sizes, transparencies, etc.
+
+# Why are the aesthetics for x, y, fill, and shape formatted the way they are? 
+     ## They are created like an object that is referenced in the server function
+     ## function, and then are given a name that is shown to the user to describe 
+     ## what the variable is. Then the filtering or choices are listed to show 
+     ## all the options, and the last row shows what is selected for the output. 
+     ## This is formatted this way to tell the output function what to reference
+     ## for the main panel. 
+
+# Note: the data frame has a "()" after it. This is necessary for reactive formatting.
+     ## Okay, good to know.
+
+# Adjust the aesthetics, playing with different shapes, colors, fills, sizes, transparencies, etc.
+     ## I tried out a transparency of 0.6 instead of 0.8, turned on the viridis color palette, 
+     ## increased the point sizes to 4 instead of 2, and changed the theme_classic base_theme 
+     ## size from 14 to 20 and didn't see a difference in the output. 
+
 #5. Analyze the code used for the renderTable function. 
-    # Notice where each bit of code comes from in the UI and server. 
-    # Note: renderTable doesn't work well with dates. "sampledate" appears as # of days since 1970.
+
+# Notice where each bit of code comes from in the UI and server. 
+     ## Pulled the data from the filtered nutrient data. 
+
+# Note: renderTable doesn't work well with dates. "sampledate" appears as # of days since 1970.
+     ## Okay. 
+
